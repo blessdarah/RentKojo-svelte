5e1f260448352944c801139faacdad1eb80780fa
= <blessdarahuba@gmail.com>
2023-07-17 04:56:36 +0100

    chore: dashboard update

--- src/routes/(admin)/dashboard/profile/+page.svelte
+++ src/routes/(admin)/dashboard/profile/+page.svelte
@@ -1,11 +1,10 @@
 <script>
-	import Counter from "../../../../components/Counter.svelte";
+	import Counter from '../../../../components/Counter.svelte';
 </script>
 
 <h3>Profile page</h3>
 <p>Manage user profile info here</p>
 <div>
-    <button>Working just fine</button>
-    <Counter />
+	<button>Working just fine</button>
+	<Counter />
 </div>
-
