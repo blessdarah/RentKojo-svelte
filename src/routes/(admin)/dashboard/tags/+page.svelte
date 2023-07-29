102eecb4e71f936281d24f95ff9f0279275e3bd9
= <blessdarahuba@gmail.com>
2023-07-29 01:00:36 +0100

    feat: add tag click to detail

--- src/routes/(admin)/dashboard/tags/+page.svelte
+++ src/routes/(admin)/dashboard/tags/+page.svelte
@@ -1,20 +1,15 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 	import Table from '../../../../components/Table.svelte';
-	import Modal from '../../../../components/Modal.svelte';
-	import TagForm from '../../../../components/tags/TagForm.svelte';
+	import { tagStore } from '../../../../store';
+	import { goto } from '$app/navigation';
+	import type { Tag } from '../../../../models/tag';
 	export let data: PageData;
 
-	let isOpen = false;
+	const handleClick = (tag: Tag) => {
+		tagStore.set(tag);
+		goto(`tags/${tag.id}`);
+	};
 </script>
 
-<Modal open={isOpen} onClose={() => (isOpen = false)}>
-	<TagForm />
-</Modal>
-<button on:click={() => (isOpen = true)}>Toggle modal</button>
-
-<Table
-	rowClick={() => (isOpen = true)}
-	columns={[{ title: 'Name', dataIndex: 'name' }]}
-	data={data.tags}
-/>
+<Table rowClick={handleClick} columns={[{ title: 'Name', dataIndex: 'name' }]} data={data.tags} />
